1aabfbdc75f529773a02732dd4b86c4a6011792b
Mounika <manthamounika@gmail.com>
2024-03-22 23:32:51 +0530

    Star pattern was added

---
+++ patterns/Starpattern.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class Starpattern {
+    public static void pattern6(int n){
+        for(int row=1;row<=n;row++){
+            System.out.print(" ".repeat(n-row));
+            System.out.print("*".repeat(row));
+            System.out.println();
+            
+        }
+    }
+    public static void main(String[] args) {
+        pattern6(6);
+    }
+}

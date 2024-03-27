1fc7133d60a5cf0e9b6ae67d1562b0ce6f40db7d
Mounika <manthamounika@gmail.com>
2024-03-27 16:49:47 +0530

    nnum pattern file was added

---
+++ patterns/numpattern.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class numpattern {
+    public static void num(int n){//correction of patterns
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(col);
+            }
+            for(int spaces=1;spaces<=2*n-(2*row);spaces++){
+                System.out.print(" ");
+            }
+            for(int col=row;col>=1;col--){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }  
+    public static void main(String[] args) {
+        num(5);
+    }  
+}
